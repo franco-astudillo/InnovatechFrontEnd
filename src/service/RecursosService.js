4afe574bae1b531211e8b11e2d5fd172846d9f3f
Oscar Astudillo <osca.astudillo@duocuc.cl>
2026-05-07 20:01:15 -0400

    feat: Instalación de axios para conexion con apiGateway en apiservice, se añade get usuarios y page de recursos

--- src/service/RecursosService.js
+++ src/service/RecursosService.js
@@ -7,7 +7,7 @@ export const RecursosService = {
       const response = await api.get('api/v1/usuarios');
       return response.data;
     } catch (error) {
-      console.error("Error al obtener recursos:", error);
+      console.error("Error al obtener recursos: ", error);
       throw error;
     }
   }
